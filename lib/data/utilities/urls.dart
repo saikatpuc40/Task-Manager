b87f2e4e8f2e510081fd6937378ff282cbfba0c9
Saikat Barua <saikatbarua2196@gmail.com>
2026-02-09 22:08:24 +0600

    complete api call in completed task,in progress ,cancelled task screen

--- lib/data/utilities/urls.dart
+++ lib/data/utilities/urls.dart
@@ -4,8 +4,11 @@ class Urls{
   static const String login = '$_baseUrl/login';
   static const String createTask ='$_baseUrl/createTask';
   static const String newTask ='$_baseUrl/listTaskByStatus/New';
+  static const String completedTask ='$_baseUrl/listTaskByStatus/Completed';
+  static const String inProgressTask ='$_baseUrl/listTaskByStatus/In Progress';
+  static const String cancelledTask ='$_baseUrl/listTaskByStatus/Cancelled';
   static const String taskStatusCount ='$_baseUrl/taskStatusCount';
   static const String profileUpdate ='$_baseUrl/profileUpdate';
   static String deleteTask(String id) => '$_baseUrl/deleteTask/$id';
-  static String completedTask(String id,String status) => '$_baseUrl/updateTaskStatus/$id/$status';
+  static String updateTask(String id,String status) => '$_baseUrl/updateTaskStatus/$id/$status';
 }
--- lib/ui/screens/cancelled_task_screen.dart
+++ lib/ui/screens/cancelled_task_screen.dart
@@ -1,5 +1,11 @@
 
 import 'package:flutter/material.dart';
+import 'package:task_manager/data/models/network_response.dart';
+import 'package:task_manager/data/models/task_list_wrapper_model.dart';
+import 'package:task_manager/data/models/task_model.dart';
+import 'package:task_manager/data/network_caller/network_caller.dart';
+import 'package:task_manager/data/utilities/urls.dart';
+import 'package:task_manager/ui/widgets/snack_bar_message.dart';
 import 'package:task_manager/ui/widgets/task_item.dart';
 
 class CancelledTaskScreen extends StatefulWidget {
@@ -10,16 +16,63 @@ class CancelledTaskScreen extends StatefulWidget {
 }
 
 class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
+  bool _getCancelledTaskScreenInProgress = false;
+  List<TaskModel> cancelledTask = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getcancelledTask();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-         // return TaskItem();
-
+      body:RefreshIndicator(
+        onRefresh: () async {
+          _getcancelledTask();
         },
+        child: Visibility(
+          visible: _getCancelledTaskScreenInProgress==false,
+          replacement: const Center(
+            child: CircularProgressIndicator(),
+          ),
+          child: ListView.builder(
+            itemCount: cancelledTask.length,
+            itemBuilder: (context, index) {
+              return TaskItem(
+                  taskModel: cancelledTask[index],
+                  onUpdate: () {
+                    _getcancelledTask();
+
+              },);
+
+            },
+          ),
+        ),
       ),
     );
+  }
+  Future<void> _getcancelledTask() async {
+    _getCancelledTaskScreenInProgress = true;
+    if(mounted){
+      setState(() {});
+    }
+    NetworkResponse response = await NetworkCaller.getRequest(Urls.cancelledTask);
+    if(response.isSuccess){
+      TaskListWrapperModel taskListWrapperModel = TaskListWrapperModel.fromJson(response.responseData);
+      cancelledTask = taskListWrapperModel.taskList??[];
+    }
+    else{
+      if(mounted){
+        showSnackBarMessage(context, response.errorMessage?? 'Get Completed Task Failed!');
+      }
+    }
+    _getCancelledTaskScreenInProgress = false;
+    if(mounted){
+      setState(() {});
+    }
+
+
   }
 }
